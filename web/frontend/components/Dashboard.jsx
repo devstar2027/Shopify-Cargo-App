4a6635c264808c8bd4d99c125ce52fd2f0ce432f
devstar2027 <adhrit.online@gmail.com>
2022-12-18 12:27:31 -0800

    feat: create shipment

--- web/frontend/components/Dashboard.jsx
+++ web/frontend/components/Dashboard.jsx
@@ -2,29 +2,93 @@ import {Card, TextStyle, Heading, List, Grid, EmptyState, Button} from '@shopify
 import React from 'react';
 import { useAuthenticatedFetch, useAppQuery } from "../hooks";
 import { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from "react";
 
 export default function Dashboard() {
 	const fetch = useAuthenticatedFetch();
+  const [shipmentId, setShipmentId] = useState(0);
+
+  // create shipment
+  const createShipment = async () => {
+    axios.post('https://api.carg0.co.il/Webservice/CreateShipment', {
+      Method: 'Ship',
+      Params: {
+        shipping_type: 1,
+        to_address: {
+          name: 'John Doe',
+          company: 'customer company',
+          street1: 'aluf david 171',
+          street2: '',
+          entrance: '',
+          floor: '',
+          appartment: '',
+          city: 'RAMAT GAN',
+          state: 'IL',
+          zip: '90210',
+          country: 'IL',
+          phone: '052249297',
+          email: 'johndoe@gmail.com'
+        },
+        from_address: {
+          name: 'John Doe',
+          company: 'my company',
+          street1: 'aluf david 171',
+          street2: '',
+          entrance: '',
+          floor: '',
+          appartment: '',
+          city: 'ramat gan',
+          state: 'il',
+          zip: '90210',
+          country: 'il',
+          phone: '039300039',
+          email: 'johndoe@gmail.com'
+        },
+        noOfParcel: 0,
+        barcode: '',
+        return_order: '2365',
+        doubleDelivery: 1,
+        TotalValue: 123,
+        TransactionID: 'ORDER2365',
+        ContentDescription: 'Milk',
+        CaashOnDeliveryTypes: 0,
+        CarrierName: 'CARGO EXPRESS',
+        CarrierService: 'CARGO',
+        CarrierID: 1,
+        OrderID: '',
+        PaymentMethod: 'Paypal',
+        Note: 'Ship by 25/06/2018',
+        customerCode: 2808
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+      setShipmentId(response.data.shipmentId);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+
+
 
   const variables = {
     id: 5225465741603,
 	}  
 
-  
-	useEffect(async () => {
-		const response = await fetch("/api/ordersList", {
-			method: "POST",
-			body: JSON.stringify({ variables }),
-			headers: { "Content-Type": "application/json" },
-		});
+	// useEffect(async () => {
+	// 	const response = await fetch("/api/ordersList", {
+	// 		method: "POST",
+	// 		body: JSON.stringify({ variables }),
+	// 		headers: { "Content-Type": "application/json" },
+	// 	});
 
-		if (response.ok) {
-			const res = await response.json()
-			const orders = res.body.data.orders
-		}
-	}, []);
-  
-
+	// 	if (response.ok) {
+	// 		const res = await response.json()
+	// 		const orders = res.body.data.orders
+	// 	}
+	// }, []);
 
   return (
     <Card
@@ -58,7 +122,7 @@ export default function Dashboard() {
                 <div>Delivery type</div>
               </Grid.Cell>
             </Grid>
-            <Button submit>Submit</Button>
+            <Button onClick={createShipment}>Submit</Button>
           </Card.Section>
         </Card>
         
